8c7abeb53ad5bb1183d44cdc8aa5b65fab059139
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-07-18 01:21:33 +0300

    added delete function

--- task-project/src/Components/TaskShow.jsx
+++ task-project/src/Components/TaskShow.jsx
@@ -1,4 +1,7 @@
-function TaskShow({ task }) {
+function TaskShow({ task, onDelete }) {
+  const handleDeleteClick = () => {
+    onDelete(task.id);
+  };
   console.log(task);
   return (
     <div className="task-show">
@@ -7,7 +10,9 @@ function TaskShow({ task }) {
       <h3 className="task-title">Yapılacaklar:</h3>
       <p>{task.taskDesc}</p>
       <div>
-        <button className="task-delete">Sil</button>
+        <button className="task-delete" onClick={handleDeleteClick}>
+          Sil
+        </button>
         <button className="task-edit">Güncelle</button>
       </div>
     </div>
