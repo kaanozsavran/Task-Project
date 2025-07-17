a9450a2cb1ac028af3e8299bf364899560564677
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-07-18 00:27:16 +0300

    adding tasks to the array and passing them as props to subcomponents

--- task-project/src/App.jsx
+++ task-project/src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./Components/TaskList";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const createTask = (title, taskDesc) => {
-    console.log(title, taskDesc);
+    const createdTasks = [
+      ...tasks,
+      {
+        id: Math.round(Math.random() * 999999),
+        title,
+        taskDesc,
+      },
+    ];
+    setTasks(createdTasks);
   };
   return (
     <div className="App">
       <TaskCreate onCreate={createTask} />
       <h1>Görevler</h1>
-      <TaskList />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
