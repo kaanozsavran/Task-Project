ba245db61af2fd3deabe93bcd164c2e934d03a32
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-07-18 00:04:47 +0300

    added feature to add task

--- task-project/src/Components/TaskCreate.jsx
+++ task-project/src/Components/TaskCreate.jsx
@@ -1,13 +1,38 @@
-function TaskCreate() {
+import { useState } from "react";
+
+function TaskCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDesc] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleTaskChange = (event) => {
+    setTaskDesc(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title, taskDesc);
+    setTaskDesc("");
+    setTitle("");
+  };
   return (
     <div className="task-create">
       <h3>Lütfen Task Ekleyiniz!</h3>
       <form className="task-form">
         <label className="task-label">Başlık</label>
-        <input className="task-input" />
+        <input value={title} className="task-input" onChange={handleChange} />
         <label className="task-label">Task Giriniz!</label>
-        <textarea className="task-input" rows={5}></textarea>
-        <button className="task-button">Oluştur</button>
+        <textarea
+          value={taskDesc}
+          className="task-input"
+          rows={5}
+          onChange={handleTaskChange}
+        ></textarea>
+        <button className="task-button" onClick={handleSubmit}>
+          Oluştur
+        </button>
       </form>
     </div>
   );
